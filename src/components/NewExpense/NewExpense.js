168ce7ec59a71217b1bb4b396c1a0cec7a1940ae
Derek Ewell <derekewell@gmail.com>
2022-03-22 22:43:18 -0600

    Expense Form

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './NewExpense.css';
+import ExpenseForm from './ExpenseForm';
 
 const NewExpense = () => {
-    return <div className='new-expense'>
-        <form></form>
-    </div>
+    return (
+        <div className='new-expense'>
+            <ExpenseForm />
+        </div>
+    );
 };
 
 export default NewExpense;
